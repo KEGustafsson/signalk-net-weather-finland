d49991a886d0cbb80725e738e3111e59d28b1b52
Teppo Kurki <teppo.kurki@iki.fi>
2024-03-17 17:47:45 +0200

    feature: add shortname

--- index.js
+++ index.js
@@ -95,57 +95,57 @@ module.exports = function createPlugin(app) {
   }
 
   const stations = [
-    ["Kotka Haapasaari", 101042, 60.29, 27.18],
-    ["Kotka Rankki", 101030, 60.38, 26.96],
-    ["Loviisa Orrengrund", 101039, 60.27, 26.45],
-    ["Porvoo Kilpilahti satama", 100683, 60.3, 25.55],
-    ["Porvoo Emäsalo", 101023, 60.2, 25.63],
-    ["Porvoo Kalbådagrund", 101022, 59.99, 25.6],
-    ["Helsinki Vuosaari satama", 151028, 60.21, 25.2],
-    ["Sipoo Itätoukki", 105392, 60.1, 25.19],
-    ["Helsinki Harmaja", 100996, 60.11, 24.98],
-    ["Helsinki Helsingin majakka", 101003, 59.95, 24.93],
-    ["Kirkkonummi Mäkiluoto", 100997, 59.92, 24.35],
-    ["Inkoo Bågaskär", 100969, 59.93, 24.01],
-    ["Raasepori Jussarö", 100965, 59.82, 23.57],
-    ["Hanko Tulliniemi", 100946, 59.81, 22.91],
-    ["Hanko Russarö", 100932, 59.77, 22.95],
-    ["Kemiönsaari Vänö", 100945, 59.87, 22.19],
-    ["Parainen Utö", 100908, 59.78, 21.37],
-    ["Kökar Bogskär", 100921, 59.5, 20.35],
-    ["Turku Rajakari", 100947, 60.38, 22.1],
-    ["Parainen Fagerholm", 100924, 60.11, 21.7],
-    ["Kumlinge kirkonkylä", 100928, 60.26, 20.75],
-    ["Lumparland Långnäs satama", 151048, 60.12, 20.3],
-    ["Maarianhamina Länsisatama", 151029, 60.09, 19.93],
-    ["Maarianhamina Lotsberget", 107383, 60.09, 19.94],
-    ["Lemland Nyhamn", 100909, 59.96, 19.95],
-    ["Hammarland Märket", 100919, 60.3, 19.13],
-    ["Kustavi Isokari", 101059, 60.72, 21.03],
-    ["Rauma Kylmäpihlaja", 101061, 61.14, 21.3],
-    ["Pori Tahkoluoto satama", 101267, 61.63, 21.38],
-    ["Kristiinankaupunki Majakka", 101268, 62.2, 21.17],
-    ["Kaskinen Sälgrund", 101256, 62.33, 21.19],
-    ["Korsnäs Bredskäret", 101479, 62.93, 21.18],
-    ["Maalahti Strömmingsbådan", 101481, 62.98, 20.74],
-    ["Mustasaari Valassaaret", 101464, 63.44, 21.07],
-    ["Pietarsaari Kallan", 101660, 63.75, 22.52],
-    ["Kokkola Tankar", 101661, 63.95, 22.85],
-    ["Kalajoki Ulkokalla", 101673, 64.33, 23.45],
-    ["Raahe Nahkiainen", 101775, 64.61, 23.9],
-    ["Raahe Lapaluoto satama", 101785, 64.67, 24.41],
-    ["Oulu Vihreäsaari satama", 101794, 65.01, 25.39],
-    ["Hailuoto Marjaniemi", 101784, 65.04, 24.56],
-    ["Kemi I majakka", 101783, 65.39, 24.1],
-    ["Kemi Ajos", 101846, 65.67, 24.52],
-    ["Asikkala Pulkkilanharju", 101185, 61.27, 25.52],
-    ["Luhanka Judinsalo", 101362, 61.7, 25.51],
-    ["Tampere Siilinkari", 101311, 61.52, 23.75],
-    ["Lappeenranta Hiekkapakka", 101252, 61.2, 28.47],
-    ["Rantasalmi Rukkasluoto", 101436, 62.06, 28.57],
-    ["Liperi Tuiskavanluoto", 101628, 62.55, 29.67],
-    ["Kuopio Ritoniemi", 101580, 62.8, 27.9],
-    ["Inari Seitalaassa", 129963, 69.05, 27.76]
+    ["Kotka Haapasaari", "Haapasaari", 101042, 60.29, 27.18],
+    ["Kotka Rankki", "Rankki", 101030, 60.38, 26.96],
+    ["Loviisa Orrengrund", "Orrengrund", 101039, 60.27, 26.45],
+    ["Porvoo Kilpilahti satama", "Kilpilahti", 100683, 60.3, 25.55],
+    ["Porvoo Emäsalo", "Emäsalo", 101023, 60.2, 25.63],
+    ["Porvoo Kalbådagrund", "Kalbådagrund", 101022, 59.99, 25.6],
+    ["Helsinki Vuosaari satama", "Vuosaari", 151028, 60.21, 25.2],
+    ["Sipoo Itätoukki", "Itätoukki", 105392, 60.1, 25.19],
+    ["Helsinki Harmaja", "Harmaja", 100996, 60.11, 24.98],
+    ["Helsinki Helsingin majakka", "Helsingin majakka", 101003, 59.95, 24.93],
+    ["Kirkkonummi Mäkiluoto", "Mäkiluoto", 100997, 59.92, 24.35],
+    ["Inkoo Bågaskär", "Bågaskär", 100969, 59.93, 24.01],
+    ["Raasepori Jussarö", "Jussarö", 100965, 59.82, 23.57],
+    ["Hanko Tulliniemi", "Tulliniemi", 100946, 59.81, 22.91],
+    ["Hanko Russarö", "Russarö", 100932, 59.77, 22.95],
+    ["Kemiönsaari Vänö", "Vänö", 100945, 59.87, 22.19],
+    ["Parainen Utö", "Utö", 100908, 59.78, 21.37],
+    ["Kökar Bogskär", "Bogskär", 100921, 59.5, 20.35],
+    ["Turku Rajakari", "Rajakari", 100947, 60.38, 22.1],
+    ["Parainen Fagerholm", "Fagerholm", 100924, 60.11, 21.7],
+    ["Kumlinge kirkonkylä", "Kumlinge", 100928, 60.26, 20.75],
+    ["Lumparland Långnäs satama", "Långnäs", 151048, 60.12, 20.3],
+    ["Maarianhamina Länsisatama", "Maarianhamina", 151029, 60.09, 19.93],
+    ["Maarianhamina Lotsberget", "Lotsberget", 107383, 60.09, 19.94],
+    ["Lemland Nyhamn", "Nyhamn", 100909, 59.96, 19.95],
+    ["Hammarland Märket", "Märket", 100919, 60.3, 19.13],
+    ["Kustavi Isokari", "Isokari", 101059, 60.72, 21.03],
+    ["Rauma Kylmäpihlaja", "Kylmäpihlaja", 101061, 61.14, 21.3],
+    ["Pori Tahkoluoto satama", "Tahkoluoto", 101267, 61.63, 21.38],
+    ["Kristiinankaupunki Majakka", "Kristiinankaupunki", 101268, 62.2, 21.17],
+    ["Kaskinen Sälgrund", "Sälgrund", 101256, 62.33, 21.19],
+    ["Korsnäs Bredskäret", "Bredskäret", 101479, 62.93, 21.18],
+    ["Maalahti Strömmingsbådan", "Strömmingsbådan", 101481, 62.98, 20.74],
+    ["Mustasaari Valassaaret", "Valassaaret", 101464, 63.44, 21.07],
+    ["Pietarsaari Kallan", "Kallan", 101660, 63.75, 22.52],
+    ["Kokkola Tankar", "Tankar", 101661, 63.95, 22.85],
+    ["Kalajoki Ulkokalla", "Ulkokalla", 101673, 64.33, 23.45],
+    ["Raahe Nahkiainen", "Nahkiainen", 101775, 64.61, 23.9],
+    ["Raahe Lapaluoto satama", "Lapaluoto", 101785, 64.67, 24.41],
+    ["Oulu Vihreäsaari satama", "Vihreäsaari", 101794, 65.01, 25.39],
+    ["Hailuoto Marjaniemi", "Marjaniemi", 101784, 65.04, 24.56],
+    ["Kemi I majakka", "Kemi I", 101783, 65.39, 24.1],
+    ["Kemi Ajos", "Ajos", 101846, 65.67, 24.52],
+    ["Asikkala Pulkkilanharju", "Pulkkilanharju", 101185, 61.27, 25.52],
+    ["Luhanka Judinsalo", "Judinsalo", 101362, 61.7, 25.51],
+    ["Tampere Siilinkari", "Siilinkari", 101311, 61.52, 23.75],
+    ["Lappeenranta Hiekkapakka", "Hiekkapakka", 101252, 61.2, 28.47],
+    ["Rantasalmi Rukkasluoto", "Rukkasluoto", 101436, 62.06, 28.57],
+    ["Liperi Tuiskavanluoto", "Tuiskavanluoto", 101628, 62.55, 29.67],
+    ["Kuopio Ritoniemi", "Ritoniemi", 101580, 62.8, 27.9],
+    ["Inari Seitalaassa", "Seitalaassa", 129963, 69.05, 27.76]
   ]
 
   readMeteo = function readMeteo() {
@@ -158,13 +158,13 @@ module.exports = function createPlugin(app) {
       }
       distToStation = []
       const ownLocation = { latitude: ownLat, longitude: ownLon }
-      stations.forEach(([stationName, fmisid, lat, lon]) => {
+      stations.forEach(([longName, shortName, fmisid, lat, lon]) => {
         const distance = haversine(ownLocation, { latitude: lat, longitude: lon })
-        distToStation.push([stationName, fmisid, lat, lon, distance])
+        distToStation.push([longName, shortName, fmisid, lat, lon, distance])
       })
-      distToStation = distToStation.sort((a, b) => a[4] - b[4]).slice(0, numberOfStations)
+      distToStation = distToStation.sort((a, b) => a[5] - b[5]).slice(0, numberOfStations)
       app.debug(distToStation)
-      distToStation.forEach(([stationName, fmisid, lat, lon, distance]) => {
+      distToStation.forEach(([longName, shortName, fmisid, lat, lon, distance]) => {
         const url = `https://tuuleeko.fi/fmiproxy/nearest-observations?lat=${lat}&lon=${lon}&latest=true`
         fetch(url, { method: 'GET' })
           .then((res) => {
@@ -198,7 +198,10 @@ module.exports = function createPlugin(app) {
                     },
                     {
                       path: '',
-                      value: { name }
+                      value: {
+                        name,
+                        shortName
+                      }
                     },
                     {
                       path: 'environment.outside.temperature',
